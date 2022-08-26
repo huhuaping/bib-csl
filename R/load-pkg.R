b8491837ab2d07a835801215f13b4911a1f246ac
huhuaping <huhuaping01@nwsuaf.edu.cn>
2022-08-26 12:41:18 +0800

    csl word output

---
+++ R/load-pkg.R
@@ -0,0 +1,34 @@
+
+# pkg needed
+
+library('bookdown')
+library('knitr')
+#library('xlsx')
+require("openxlsx")
+#install.packages('tidyr')
+#install.packages('plyr')
+library('tidyr')
+library('dplyr')
+library('stringr')
+library('tidyverse')
+library('ggplot2')
+library("scales")
+#install.packages("gridExtra")
+library("gridExtra")
+#install.packages("magrittr")
+library("magrittr")
+#install.packages("ggthemes")
+#install.packages("ggrepel")
+require("ggthemes")
+require("ggrepel")
+require("lubridate")
+require("DT")
+require("png")
+require("jpeg")
+require("glue")
+require("foreign")
+require("here")
+
+
+# my custom pkg
+require(techme)
